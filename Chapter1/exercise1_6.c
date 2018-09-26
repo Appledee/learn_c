c02a1fb2cce9b44eb10adc2309018522903b365f
Appledee <zwy.sds@gmail.com>
2018-09-26 08:31:09 -0700

    update exercise 1.6

---
+++ Chapter1/exercise1_6.c
@@ -0,0 +1,14 @@
+//this is a program to verify that the expression getchar() != EOF is 0 or 1. 
+//
+
+#include <stdio.h>
+
+
+int main(){
+
+int c;
+while(c = (getchar() != EOF) ){
+  printf("%d\n", c);}
+printf("%d - at EOF \n");
+return 0;
+}
