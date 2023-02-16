e82c938a0a1f9f57264a96f933ef0c6d52af6ae2
Appledee <zwy.sds@gmail.com>
2023-02-15 21:03:25 -0800

    c

---
+++ Data_structure/pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char *ptr = "Linux";
+    printf("%c\n",*ptr);
+    printf("%c\n",*ptr++);
+    printf("%c\n",*ptr);
+    
+    return 0;
+    
+}
+
+
+//return: 
+//L
+//L
+//i
